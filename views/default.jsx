f9ef494920d048df39e2aa2169879d9d39f3550b
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2022-09-06 21:38:46 -0400

    Adding Bootstrap grid to index page

--- views/default.jsx
+++ views/default.jsx
@@ -1,10 +1,11 @@
 const React = require('react')
+
 function Def (html) {
     return (
         <html>
             <head>
                 <title>Title</title>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous" />
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" />
             </head>
             <body>
                 {html.children}
@@ -12,4 +13,6 @@ function Def (html) {
         </html>
     )
 }
+
 module.exports = Def
+
