c5c8d9543cdb608380ff60c846fa56f667401593
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2022-09-07 00:07:54 -0400

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -20,19 +20,17 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
-router.delete('/:id', (req, res) => {
+router.get('/:id/edit', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
-    res.render('error404')
+      res.render('error404')
   }
   else if (!places[id]) {
-    res.render('error404')
+      res.render('error404')
   }
   else {
-    places.splice(id, 1)
-    res.redirect('/places')
+    res.render('places/edit', { place: places[id] })
   }
 })
-
 
 module.exports = router
