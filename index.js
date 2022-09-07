9e5c3ae5930c27ceda63048501ec902814a6b99a
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2022-09-06 22:28:36 -0400

    Form data is getting through!

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.use(express.urlencoded({ extended: true }))
+
 app.use('/places', require('./controllers/places'))
 app.get('*', (req, res) => {
     res.render('error404')
