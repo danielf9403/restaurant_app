2baa07445739de156e6b17f699520a8d8a2ee573
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2022-09-11 23:42:53 -0400

    end part 10

--- controllers/places.js
+++ controllers/places.js
@@ -31,13 +31,23 @@ router.get('/:id', (req, res) => {
   res.send('GET /places/:id stub')
 })
 
-router.get('/:id', (req, res) => {
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
   db.Place.findById(req.params.id)
   .then(place => {
-      res.render('places/show', { place })
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
   })
   .catch(err => {
-      console.log('err', err)
       res.render('error404')
   })
 })
